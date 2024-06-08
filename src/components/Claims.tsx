0f872c0bd90a39e5897ab942b660fc32d97e2b7f
shajib126 <shajib126@gmail.com>
2024-06-08 21:09:16 +0600

    update claim comp

--- src/components/Claims.tsx
+++ src/components/Claims.tsx
@@ -12,7 +12,7 @@ const Claims = () => {
   return (
     <div className="mt-8">
         {isLoading?<Loading/> : data?.data?.length <= 0 ?<h1 className="text-rose-500"> Claim not found</h1> : <div className="w-[80%] mx-auto md:flex flex-wrap gap-4">
-            {data?.data?.map((claim:any,i:number)=><ClaimCard key={i} claim={claim} />)}
+            {data?.data?.claims?.map((claim:any,i:number)=><ClaimCard key={i} claim={claim} />)}
         </div>}
     </div>
   )
